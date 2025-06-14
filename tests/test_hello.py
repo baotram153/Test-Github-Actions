c4c4f1e0b433fa86c22951068607fcf664ed0365
baotram153 <tramdang7907129@gmail.com>
2025-06-14 15:07:47 +0700

    Merge branch.:

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,4 +4,4 @@ def test_hello():
     assert hello() == "Hello, world!", "Function hello() should return 'Hello, world!'"
 
 def test_hello_false():
-    assert hello() != "Hello, world", "Test fail testcase."
+    assert hello() != "Hello, world!", "Test fail testcase."
