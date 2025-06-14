3d1c76ffd97081da0be48b156625a76f249c36fb
baotram153 <tramdang7907129@gmail.com>
2025-06-14 15:03:52 +0700

    Test fail testcase.

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,3 +2,6 @@ from src.hello import hello
 
 def test_hello():
     assert hello() == "Hello, world!", "Function hello() should return 'Hello, world!'"
+
+def test_hello_false():
+    assert hello() != "Hello, world", "Test fail testcase."
